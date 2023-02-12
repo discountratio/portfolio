a9f8de87c914878f682809418e6e2d499082b98c
discountratio <discountratio@gmail.com>
2023-02-12 16:53:44 +0700

    fixing projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,148 +1,16 @@
 export const projects = [
+
   {
     id: 1,
-    title: "Calculator",
-    bigIcon: "fa-solid fa-calculator",
-    description: "Calculations, event listeners, and DOM manipulation",
+    title: "User Page for a Language Learning App",
+    bigIcon: " fa-solid fa-language",
+    description: "  Fully responsive landing page for a user profile on a language learning website that fetches country data from a REST API for basic stats and language information. Features dynamically created elements from a JSON file and uses custom components.",
     icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://github.com/discountratio/calculator",
-    preview:
-      "https://htmlpreview.github.io/?https://github.com/discountratio/calculator/blob/main/calculator.html",
-  },
-  {
-    id: 2,
-    title: "Rabbit Artist",
-    bigIcon: " fa-solid fa-carrot",
-    description: "A website and shop for an AI created artist.",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
     ],
     link: "https://github.com/discountratio/rabbit-website",
     preview: "https://rabbitsforsale.netlify.app/",
   },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    bigIcon: "html",
-    description: "A cool project",
-    icons: [
-      "fa-brands fa-html5",
-      "fa-brands fa-square-js",
-      "fa-brands fa-sass",
-    ],
-    link: "https://www.google.com",
-    preview: "",
-  },
+
 ];
 
 export default projects;
